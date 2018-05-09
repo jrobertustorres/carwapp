1b15cf2fdcbbe7b67bca866fa13a720cdc4ebe08
jose <jrobertustorres@gmail.com>
2018-05-09 18:54:20 -0300

    correção erro loading fornecedores e mapa

---
+++ .history/src/providers/fornecedor-service_20180509143550.ts
@@ -0,0 +1,114 @@
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Constants } from '../app/constants';
+import { Storage } from '@ionic/storage';
+
+//ENTITYS
+// import { FornecedorEntity } from '../model/fornecedor-entity';
+
+
+@Injectable()
+export class FornecedorService {
+
+  private headers = new Headers({ 'Content-Type': 'application/json' });
+  private options = new RequestOptions({ headers: this.headers, method: "post" });
+  // private fornecedorEntity: FornecedorEntity;
+
+  constructor(public _http: Http,
+              private _storage: Storage) {
+  }
+
+    // public findFornecedorPorRaio () {
+    //     try {
+
+    //       return new Promise((resolve, reject) => {
+    //         this._storage.get(Constants.TOKEN_USUARIO).then((tokenUsuario) => {
+    //           this._http.post(Constants.API_URL + 'findFornecedorPorRaio /'
+    //             + tokenUsuario, this.options)
+    //             .map(res=>res.json())
+    //             .subscribe(data => {
+    //               resolve(data);
+    //             }, (err) => {
+    //               // reject(err.json());
+    //             });
+    //         });
+    //       });
+    
+    //     } catch (e){
+    //       if(e instanceof RangeError){
+    //         console.log('out of range');
+    //       }
+    //     }
+    // }
+
+    public findFornecedorPorRaio(fornecedorEntity) {
+        try {
+            return new Promise((resolve, reject) => {
+              
+              this._storage.get(Constants.TOKEN_USUARIO).then((tokenUsuario) => {
+                this._http.post(Constants.API_URL + 'findFornecedorPorRaio/'
+                  + tokenUsuario, JSON.stringify(fornecedorEntity), this.options)
+                  .map(res=>res.json())
+                  .subscribe(data => {
+                    resolve(data);
+                  }, (err) => {
+                  });
+              });
+            });
+    
+        } catch (e){
+          if(e instanceof RangeError){
+            console.log('out of range');
+          }
+        }
+    }
+
+    public findDadosFornecedor (fornecedorEntity) {
+        try {
+            // this.fornecedorEntity = new FornecedorEntity();
+            // this.fornecedorEntity.idFornecedor = idFornecedor;
+
+            // console.log(this.fornecedorEntity);
+
+            return new Promise((resolve, reject) => {
+              this._storage.get(Constants.TOKEN_USUARIO).then((tokenUsuario) => {
+                this._http.post(Constants.API_URL + 'findDadosFornecedor /'
+                  + tokenUsuario, JSON.stringify(fornecedorEntity), this.options)
+                  .map(res=>res.json())
+                  .subscribe(data => {
+                    resolve(data);
+                  }, (err) => {
+                    // reject(err.json());
+                  });
+              });
+            });
+    
+        } catch (e){
+          if(e instanceof RangeError){
+            console.log('out of range');
+          }
+        }
+    }
+
+    public imagemFornecedor(fornecedorEntity) {
+      try {
+          return new Promise((resolve, reject) => {
+            this._storage.get(Constants.TOKEN_USUARIO).then((tokenUsuario) => {
+              this._http.post(Constants.API_URL + 'imagemFornecedor/'
+                + tokenUsuario, JSON.stringify(fornecedorEntity), this.options)
+                .map(res=>res.json())
+                .subscribe(data => {
+                  resolve(data);
+                }, (err) => {
+                  // reject(err.json());
+                });
+            });
+          });
+  
+      } catch (e){
+        if(e instanceof RangeError){
+          console.log('out of range');
+        }
+      }
+  }
+}
